43dfeb2a7848c35e6434cbe84dde70ddf21b40bc
NagyGellaiGergo <nggeriii@gmail.com>
2025-05-13 14:40:46 +0200

    3. feladat
    Service class implemented

---
+++ src/main/java/hu/pte/mik/prog4/potzh/service/MovieService.java
@@ -0,0 +1,41 @@
+package hu.pte.mik.prog4.potzh.service;
+
+import hu.pte.mik.generated.ws.MovieDataRequest;
+import hu.pte.mik.generated.ws.MovieImdb;
+import hu.pte.mik.prog4.potzh.entity.MovieEntity;
+import hu.pte.mik.prog4.potzh.repository.MovieRepository;
+
+import java.util.List;
+
+public class MovieService {
+
+    private final MovieRepository movieRepository;
+
+    public MovieService(MovieRepository movieRepository) {
+        this.movieRepository = movieRepository;
+    }
+
+    public List<MovieEntity> listAll(){
+        return this.movieRepository.listAll();
+    }
+
+    public MovieEntity findById(Long id){
+        return this.movieRepository.findById(id);
+    }
+
+    public MovieEntity save(Long id, String title, String director_name, String release_year, String genre) {
+        return this.movieRepository.save(new MovieEntity(id, title, director_name, release_year, genre));
+    }
+
+    public long getMovieImdb(String movieId) {
+        MovieImdb service = new hu.pte.mik.generated.ws.MovieImdb();
+        MovieDataRequest request = new hu.pte.mik.generated.ws.MovieDataRequest();
+        request.setMovieId(movieId);
+
+        var port = service.getSoapMovieDataServicePort();
+        var resp = port.getMovieData(request);
+
+        var imdb = resp.getImdb();
+        return imdb;
+    }
+}
